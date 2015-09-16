fc753bea92326111e108088646d6a74031b95910
zhangluxin <117408802@qq.com>
2015-09-16 20:10:34 +0800

    意图实现loading

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ cc.game.onStart = function () {
     cc.view.adjustViewPort(true);
     cc.view.setDesignResolutionSize(ct.width, ct.height, ct.resolutionPolicy);
     cc.view.resizeWithBrowserSize(true);
-    //cc.LoaderScene.preload(g_resources, function () {
-    //    cc.director.runScene(new view.Logo());
-    //}, this);
     cc.director.runScene(new LogoScene());
 };
 cc.game.run();
--- src/views/scenes/GameLoadingScene.js
+++ src/views/scenes/GameLoadingScene.js
@@ -2,5 +2,5 @@
  * Created by zhangluxin on 15/9/16.
  */
 var GameLoadingScene = cc.Scene.extend({
-    
+
 });
